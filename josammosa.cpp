a2e1fba74d5fb2c6498b6490a0558ab0ab7a189d
RyuSeongho <87190298+RyuSeongho@users.noreply.github.com>
2023-01-31 19:14:05 +0900

    Update josammosa.cpp

--- josammosa.cpp
+++ josammosa.cpp
@@ -1,54 +1,37 @@
 #include <iostream>
-#include <limits>
 using namespace std;
 
 int n;
 int p[20][20] = {0};
-bool arr[20];
+bool arr[20] = {0};
 int mmin = 2147483647;
 
-void cal(){
-
-}
-
 void getmin(){
-    for(int i=0; i< n; i++){
-        cout << arr[i] << " ";
-    }
     int tsum = 0;
     int fsum = 0;
     for(int i=0; i < n; i++){
-        if(arr[i]){
-            for(int j=0; j < n; j++){
-                if(arr[j]) tsum += arr[j];
-            }
-        } else {
-            for(int j=0; j < n; j++){
-                if(!arr[j]) fsum += arr[j];
+        for(int j=0; j < n; j++){
+            if(arr[i] == arr[j]){
+                if(arr[i]) tsum += p[i][j];
+                else fsum += p[i][j];
             }
         }
     }
-    cout << endl << fsum << " ";
-    cout << tsum << endl << endl;
     int cmin = abs(fsum - tsum);
     if(cmin < mmin) mmin = cmin;
 }
 
-void dfs(int size, int indic, bool isSame){
-    if(indic >= n && size != n/2) return;
-    if(size < n/2){ //아직 다 안 찼으면
-        if(isSame) arr[indic] = arr[indic-1];
-        else arr[indic] = !arr[indic-1];
-        if(arr[indic]) size++;
-        dfs(size, indic + 1, true);
-        dfs(size, indic + 1, false);
-    } else { //true가 다 차면
-        for(; indic < n; indic++){
-            arr[indic] = false;
-        }
+void dfs(int idx, bool flag, int truenum){
+    int maxtruenum = n - idx + truenum;
+    if(maxtruenum < n/2) return; //halt
+    if(idx >= n){ //halt
         getmin();
+        return;
     }
-    
+    arr[idx++] = flag; //apply
+    if(flag) truenum++;
+    if(truenum < n/2) dfs(idx, true, truenum);
+    dfs(idx, false, truenum);
 }
 
 int main() {
@@ -59,9 +42,7 @@ int main() {
         }
     }
     
-    arr[0] = true;
-    dfs(1, 1, true);
-    dfs(1, 1, false);
+    dfs(0, true, 0);
 
     cout << mmin;
     return 0;
