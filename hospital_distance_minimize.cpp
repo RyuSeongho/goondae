c1e468fe19d767306fe2e9974454b9b3584c7769
RyuSeongho <87190298+RyuSeongho@users.noreply.github.com>
2023-02-20 11:22:50 +0900

    Update hospital_distance_minimize.cpp

--- hospital_distance_minimize.cpp
+++ hospital_distance_minimize.cpp
@@ -5,32 +5,11 @@ using namespace std;
 int n, m;
 int plain[50][50] = {0};
 
-class Point{
-    public:
-        int x;
-        int y;
 
-    Point(int x, int y){
-        this->x = x;
-        this->y = y;
-    }
 
-    void inc(){
-        y++;
-        if(y >= n){
-            x++;
-            y=0;
-        }
-    }
-
-    bool checkOutBoundOnlyXAbove(){
-        if(x >= m) return true;
-        return false;
-    }
-};
-
-vector<Point> patients;
-vector<Point> hospitals;
+vector<pair<int,int>> patients;
+vector<pair<int,int>> hospitals;
+vector<int> selected_hospital_index;
 
 Point gptr(0,0);
 int currsum = 0;
@@ -38,11 +17,16 @@ int gmax = 0;
 
 void distanceCalculation(){}
 
-void hospitalDistributionDfs(){
-    if(gptr.checkOutBoundOnlyXAbove()) return;
-    gptr.inc();
-    if(plain[gptr.x][gptr.y] != 2) return;
-    
+void hospitalDistributionDfs(int idx){
+    if(idx >= hospitals.size()) return;
+    if(selected_hospital_index.size() > m) {
+        distanceCalculation();
+        
+    }
+
+    hospitalDistributionDfs(idx + 1);\
+    selected_hospital_index.push_back(idx);
+
 
 }
 
@@ -51,7 +35,20 @@ int main() {
     cin >> n >> m;
 
     for(int i=0; i< n; i++){
-        for(int j=0; j<n; j++) cin >> plain[i][j];
+        for(int j=0; j<n; j++){
+            int input;
+            switch(input){
+                case 1:
+                    patients.push_back(input);
+                    break;
+                case 2:
+                    hospitals.push_back(input);
+                    break;
+                default:
+                    break;
+
+            }
+        }
     }
 
     
