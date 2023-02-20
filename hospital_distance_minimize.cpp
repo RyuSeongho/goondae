c013b19a2464797c5ccb6374716e322cee50e713
RyuSeongho <87190298+RyuSeongho@users.noreply.github.com>
2023-02-20 19:19:10 +0900

    Update hospital_distance_minimize.cpp

--- hospital_distance_minimize.cpp
+++ hospital_distance_minimize.cpp
@@ -3,31 +3,47 @@
 using namespace std;
 
 int n, m;
-int plain[50][50] = {0};
-
-
 
 vector<pair<int,int>> patients;
 vector<pair<int,int>> hospitals;
 vector<int> selected_hospital_index;
 
-Point gptr(0,0);
-int currsum = 0;
-int gmax = 0;
+int gmin = 100 * 100;
 
-void distanceCalculation(){}
-
-void hospitalDistributionDfs(int idx){
-    if(idx >= hospitals.size()) return;
-    if(selected_hospital_index.size() > m) {
-        distanceCalculation();
-        
+int getNearestHospitalDistance(int patient_idx){
+    pair<int,int> patient_loc = patients[patient_idx];
+    int fmin = 100;
+    for(int i=0; i < selected_hospital_index.size(); i++){
+        int selected_idx = selected_hospital_index[i];
+        pair<int,int> hospital_loc = hospitals[selected_idx];
+        int distance_first = abs(hospital_loc.first - patient_loc.first);
+        int distance_second = abs(hospital_loc.second - patient_loc.second);
+        int distance = distance_first + distance_second;
+        if(distance < fmin) fmin = distance;
     }
+    return fmin;
+}
 
-    hospitalDistributionDfs(idx + 1);\
+void distanceCalculation(){
+    int fsum = 0;
+    for(int i=0; i < patients.size(); i++){
+        fsum += getNearestHospitalDistance(i);
+    }
+    if(fsum < gmin) gmin = fsum;
+}
+
+void hospitalDistributionDfs(int idx, int left, int limit){
+    if(left <= 0) {
+        distanceCalculation();
+        return;
+    }
+    if(idx >= limit) return;
+
+
+    hospitalDistributionDfs(idx + 1, left, limit);
     selected_hospital_index.push_back(idx);
-
-
+    hospitalDistributionDfs(0, left - 1, idx);
+    selected_hospital_index.pop_back();
 }
 
 int main() {
@@ -37,12 +53,13 @@ int main() {
     for(int i=0; i< n; i++){
         for(int j=0; j<n; j++){
             int input;
+            cin >> input;
             switch(input){
                 case 1:
-                    patients.push_back(input);
+                    patients.push_back(make_pair(i,j));
                     break;
                 case 2:
-                    hospitals.push_back(input);
+                    hospitals.push_back(make_pair(i,j));
                     break;
                 default:
                     break;
@@ -50,7 +67,9 @@ int main() {
             }
         }
     }
-
+    
+    hospitalDistributionDfs(0, m, hospitals.size());
+    cout << gmin;
     
     return 0;
 }
